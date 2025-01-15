dbec7c72237821f3914c14601bbea4d2d30ce8e8
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-15 22:56:55 +0530

    setting up firebase

--- lib/views/contactspage.dart
+++ lib/views/contactspage.dart
@@ -11,6 +11,8 @@ class ContactsPage extends StatefulWidget {
 class _ContactsPageState extends State<ContactsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
