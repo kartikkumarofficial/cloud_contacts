d22ffc484f759d2d180926e8d91250da273d0e65
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-15 22:36:16 +0530

    Initial commit

---
+++ lib/views/contactdetailspage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Contactdetailspage extends StatefulWidget {
+  const Contactdetailspage({super.key});
+
+  @override
+  State<Contactdetailspage> createState() => _ContactdetailspageState();
+}
+
+class _ContactdetailspageState extends State<Contactdetailspage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
